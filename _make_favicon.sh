76a1bee197a9e5e7d9e150a46370f06afcd320a6
Walz Stefanie (059) <stewal@cmtcleu75929202.rd.corpintra.net>
2024-04-26 23:06:41 +0200

    update favicon

--- _make_favicon.sh
+++ _make_favicon.sh
@@ -1,1 +1,1 @@
-convert -background transparent "images/circle_bw_crop.jpg" -define icon:auto-resize=16,24,32,48,64,72,96,128,256 "favicon.ico"
+convert -background transparent "images/mercedes_benz_star.png" -define icon:auto-resize=64 "favicon.ico"
