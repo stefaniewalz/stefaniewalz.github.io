f68cd9815020737070667bb0d161b4abf9cd73b3
Walz Stefanie (059) <stewal@cmtcleu75929202.rd.corpintra.net>
2024-04-27 13:30:22 +0200

    Increase thumbnail size

--- _make_thumbnails.sh
+++ _make_thumbnails.sh
@@ -1,3 +1,3 @@
 for file in images/*.{jpg,png}; do
-    [ ! -f "tn/$file" ] && convert "$file"  -thumbnail 260x260 "tn/$file"
+    [ ! -f "tn/$file" ] && convert "$file"  -thumbnail 560x560 "tn/$file"
 done
